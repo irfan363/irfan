77966f2605130c492eaab9cadc91f29f9814788a
irfan363 <irfanzaenal318@gmail.com>
2024-11-28 13:13:34 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+function tampilkan():void{
+    echo "saya punya mobil";
+}
+
+function perkalian($angka1,$angka2): float|int {
+    return $angka1 = $angka2;
+}
+
+tampilkan();
+echo "berjumlah:";
+echo perkalian( 8,2);
+?>
